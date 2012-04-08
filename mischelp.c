a766c7eca7a8f5290a17fea47fd449aa090ef639
Bumble-Bee <bigbeeshane@gmail.com>
2012-04-08 18:32:50 +0100

    add android logging and dont fclose on NULL

--- mischelp.c
+++ mischelp.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <utils/Log.h>
 
 #define MAX_COM_LENGTH 14
 #define LOG_TAG "mischelp"
@@ -14,19 +15,19 @@ int main(int argc, char *argv[])
 
     if (argc < 2 || argc > 3){
         printf("\nUsage : %s <path of misc> <boot command>\n\n", LOG_TAG);
+        LOGE("ERROR : Usage : %s <path of misc> <boot command>\n", LOG_TAG);
         return 0;
     } else if (argc == 2) {
-        printf("\nEraseing boot coommand to partition %s\n\n", argv[1]);
+        LOGI("Eraseing boot coommand to partition %s\n", argv[1]);
     } else if (argc == 3) {
         strncpy(bootcom, argv[2], MAX_COM_LENGTH);
-        printf("\nWriteing boot coommand \"%s\" to partition \"%s\"\n\n", bootcom, argv[1]);
+        LOGI("Writeing boot coommand \"%s\" from partition \"%s\"\n", bootcom, argv[1]);
     }
 
     FILE *f;
     f=fopen(argv[1], "wb");
     if (f == NULL) {
-        fclose(f);
-        printf("ERROR : partition not found \"%s\"\n\n", argv[1]);
+        LOGE("ERROR : partition not found \"%s\"\n", argv[1]);
         return 0;
     }
 
